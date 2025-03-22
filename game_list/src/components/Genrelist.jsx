532dcf336f9ce523f176dff91ea828e412b14ddb
govind-ik <jeeaspirant4567@gmail.com>
2025-03-22 19:48:11 +0000

    selecting the genre completed using index

--- game_list/src/components/Genrelist.jsx
+++ game_list/src/components/Genrelist.jsx
@@ -6,6 +6,8 @@ import { Themecolor } from '../context/Theme';
 const Genrelist = () => {
   const {theme,setTheme}=useContext(Themecolor);
   const [genremp,setGenremp]=useState([])
+  const [activeindex,setActiveindex]=useState(0);
+  
     useEffect(()=>{
         genrelist();
 
@@ -19,8 +21,8 @@ const Genrelist = () => {
   return (
     <div >
       <h1 className= {`font-bold  ${theme} ${theme==='light'?'text-black':' text-amber-50'}  text-3xl ml-4 mt-2 mb-2 `}>Genres</h1>
-      {genremp.map((resp)=>(
-        <div className='flex gap-2 items-centre hover:bg-gray-300 rounded-2xl group  mb-1  cursor-pointer '>
+      {genremp.map((resp,index)=>(
+        <div onClick={()=> setActiveindex(index)} className={`flex gap-2 items-centre hover:bg-gray-300 rounded-2xl group  mb-1  cursor-pointer ${activeindex===index?'bg-gray-300':null} `}>
           <img src={resp.image_background} className='w-[40px] h-[40px]   ml-5 rounded-2xl object-cover mb-2 mt-2 group-hover:scale-120 transition-all ease-out duration-300'/>
           <h1 className={`mt-4 ml-3  ${theme} ${theme==='light'?'text-black':' text-amber-50' } font-bold group-hover:scale-120 transition-all ease-out duration-300`} >{resp.name}</h1>
         </div>
