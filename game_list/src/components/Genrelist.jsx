64eaaf81f63a61d99633991d5af9fe7ccd0a0b31
govind-ik <jeeaspirant4567@gmail.com>
2025-03-22 19:26:33 +0000

    hover effect along with some effect on hover like scale

--- game_list/src/components/Genrelist.jsx
+++ game_list/src/components/Genrelist.jsx
@@ -18,9 +18,12 @@ const Genrelist = () => {
     }
   return (
     <div >
-      <h1 className= {`font-bold  ${theme} ${theme==='light'?'text-black':' text-amber-50'}  text-3xl ml-4 mt-0`}>Genres</h1>
+      <h1 className= {`font-bold  ${theme} ${theme==='light'?'text-black':' text-amber-50'}  text-3xl ml-4 mt-2 mb-2 `}>Genres</h1>
       {genremp.map((resp)=>(
-        <img src={resp.image_background} className='w-[60px] h-[60px] mt-5 ml-5 rounded-2xl'/>
+        <div className='flex gap-2 items-centre hover:bg-gray-300 rounded-2xl group  mb-1  cursor-pointer '>
+          <img src={resp.image_background} className='w-[40px] h-[40px]   ml-5 rounded-2xl object-cover mb-2 mt-2 group-hover:scale-120 transition-all ease-out duration-300'/>
+          <h1 className={`mt-4 ml-3  ${theme} ${theme==='light'?'text-black':' text-amber-50' } font-bold group-hover:scale-120 transition-all ease-out duration-300`} >{resp.name}</h1>
+        </div>
 
       ))}
       
