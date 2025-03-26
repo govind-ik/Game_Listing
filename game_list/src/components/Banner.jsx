28af9d4da7ae1adbf7bcb7a992def943f9895c28
govind-ik <jeeaspirant4567@gmail.com>
2025-03-26 20:34:57 +0000

    text and button over banner placed and absolute and relative solved

--- game_list/src/components/Banner.jsx
+++ game_list/src/components/Banner.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Banner = (props) => {
     console.log(props.bannerimage)
   return (
-    <div>
+    <div className='relative '>
+      <div className='absolute bottom-0 p-5  bg-gradient-to-t from-slate-900 to-transparent w-full'>
+        <h2 className='text-[24px] font-bold'> {props.bannerimage.name} </h2>
+        <button className='bg-blue-600 rounded-lg w-[100px] h-[30px] font-bold'>Get Game</button>
+      </div>
         <img  src={props.bannerimage.background_image
 } className='w-full object-cover rounded-xl h-[500px] mt-4' />
     </div>
